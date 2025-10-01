f460798500bf2654d47c7870694f562a5ff9b13c
krishnaprasadc <krishnaprasadc45@gmail.com>
2025-10-02 02:57:40 +0530

    Remove compiled JS files from repo

--- dist/business/errors/errorHandling.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleError = void 0;
-var handleError = function (error) {
-    throw new Error(error.message);
-};
-exports.handleError = handleError;
--- dist/framework/middleware/loggingMiddleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var loggingMiddleware = function (req, res, next) {
-    console.log("Received ".concat(req.method, " request for ").concat(req.url));
-    console.log('Request Body:', req.body);
-    next();
-};
-exports.default = loggingMiddleware;
